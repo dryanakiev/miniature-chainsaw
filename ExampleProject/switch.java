6f880c8e2f277142310c84a61384cad60d0d79c5
MSIvanov21 <MSIvanov21@codingburgas.bg>
2025-01-10 13:55:11 +0200

    Add Switch class

---
+++ ExampleProject/switch.java
@@ -0,0 +1,15 @@
+
+public class switch{
+    private char choose;
+    public switch(char choose)
+    {
+        this.choose = choose;
+    }   
+    public void switchChar(choose)
+    {
+        case 1: "This is case 1\n"; break;
+        case 2: "This is case 2\n"; break;
+        case 3: "This is case 3\n"; break;
+        default: "Invalid input.\n"; break;
+    }
+}
